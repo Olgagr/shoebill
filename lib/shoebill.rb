57ac5c63690034b3041f877c28383057582c9630
Olga Grabek <ola@codequest.eu>
2013-11-29 10:31:32 +0100

    hello world from Shoebill

--- lib/shoebill.rb
+++ lib/shoebill.rb
@@ -1,5 +1,13 @@
 require "shoebill/version"
 
 module Shoebill
-  # Your code goes here...
+
+  class Application
+
+    def call(env)
+      [200, { 'Content-Type' => 'text/html' }, ['Hello World from Shoebill']]
+    end
+
+  end
+
 end
