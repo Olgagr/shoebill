9f57a4d4ab760c2efc97a7986fa14a75cf950506
Olga Grabek <ola@codequest.eu>
2013-11-30 15:14:51 +0100

    add tests for controller and routing

--- test/test_application.rb
+++ test/test_application.rb
@@ -3,6 +3,14 @@ require_relative 'test_helper'
 class TestApp < Shoebill::Application
 end
 
+class TestController < Shoebill::Controller
+
+  def show
+    'This is a test response'
+  end
+
+end
+
 class ShoebillAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
@@ -11,8 +19,13 @@ class ShoebillAppTest < Test::Unit::TestCase
   end
 
   def test_request
-    get '/'
+    get '/test/show'
     assert last_response.ok?
-    assert_equal 'Hello World from Shoebill', last_response.body
+    assert_equal 'This is a test response', last_response.body
+  end
+
+  def test_routing
+    env = { 'PATH_INFO' => '/test/show' }
+    assert app.get_controller_and_action(env) == [TestController, 'show']
   end
 end
