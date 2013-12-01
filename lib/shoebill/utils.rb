db30fc9ddacf7faaaca726d704b9ab472dacb4c8
Olga Grabek <ola@codequest.eu>
2013-12-01 15:33:39 +0100

    add to_undescore method

--- lib/shoebill/utils.rb
+++ lib/shoebill/utils.rb
@@ -2,6 +2,14 @@ module Shoebill
 
   class Utils
 
+    def self.to_underscore(string)
+      string.gsub('::', '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+      gsub(/([a-z\d])([A-Z])/, '\1_\2').
+      tr('-', '_').
+      downcase
+    end
+
   end
 
 end
