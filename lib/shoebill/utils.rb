927b0918aa50eccdad9cfcc35cb5c4c908d33b04
Olga Grabek <ola@codequest.eu>
2013-12-21 19:09:21 +0100

    add documentation to Utils class

--- lib/shoebill/utils.rb
+++ lib/shoebill/utils.rb
@@ -4,7 +4,9 @@ module Shoebill
   class Utils
 
     # Changes CameCase string into underscore string.
-    # Additionally, it changes '::' into '/', to find diretory to the file.
+    # Additionally, it changes '::' into '/', to return diretory to the file.
+    # === Example
+    #   Utils.to_underscore(Example::CamelCase) # returns example/camel_case
     def self.to_underscore(string)
       string.gsub('::', '/').
       gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
@@ -13,7 +15,10 @@ module Shoebill
       downcase
     end
 
-    # Gets underscore name from CamelCase controller name.
+    # Gets underscore name controller without 'Controller' word from CamelCase controller name.
+    # It is used, for example, to get views folder.
+    # === Example
+    #   Utils.get_controller_name(MyCustomController) # returns my_custom
     def self.get_controller_name(name)
       klass = name.to_s.match(/^(.+)Controller/).captures[0]
       to_underscore klass
