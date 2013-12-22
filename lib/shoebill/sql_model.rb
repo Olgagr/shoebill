ddbee0bbcf1d539bfeb76dc00a1bb22bfadb7948
Olga Grabek <ola@codequest.eu>
2013-12-22 07:53:50 +0100

    add documentation

--- lib/shoebill/sql_model.rb
+++ lib/shoebill/sql_model.rb
@@ -5,9 +5,18 @@ DB = SQLite3::Database.new 'development.rb'
 
 module Shoebill
 
+  # Namespace for module.
   module Model
+
+    # Man class for SQLite model.
     class SQLite
 
+      # Returns class underscore name
+      # === Example
+      #   class MyModel < SQLite
+      #   end
+      #
+      #   MyModel.table  # returns my_model
       def self.table
         Shoebill::Utils.to_underscore name
       end
