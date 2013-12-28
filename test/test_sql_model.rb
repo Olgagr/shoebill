122acb127225f6bd5c3a6694dbedb26f8b4b8255
Olga Grabek <ola@codequest.eu>
2013-12-28 08:23:11 +0100

    add SQLModel tests

--- test/test_sql_model.rb
+++ test/test_sql_model.rb
@@ -7,16 +7,10 @@ class ShoebillTestModel < Test::Unit::TestCase
 
   def setup
     @model = TestModelSQL.new
-  end
 
-  def test_table
-    assert_equal TestModelSQL.table, 'test_model_sql'
-  end
+    @conn = SQLite3::Database::new 'development.db'
 
-  def test_schema
-    conn = SQLite3::Database::new 'development.db'
-
-    conn.execute <<SQL
+    @conn.execute <<SQL
 create table test_model_sql (
   id INTEGER PRIMARY KEY,
   posted INTEGER,
@@ -25,6 +19,20 @@ create table test_model_sql (
   description VARCHAR(32000)
 );
 SQL
+  end
+
+  def teardown
+    @conn.execute <<SQL
+DROP TABLE test_model_sql;
+SQL
+    File.delete('development.db')
+  end
+
+  def test_table
+    assert_equal TestModelSQL.table, 'test_model_sql'
+  end
+
+  def test_schema
     assert_equal TestModelSQL.schema, {
         'id' => 'INTEGER',
         'posted' => 'INTEGER',
@@ -32,12 +40,25 @@ SQL
         'link' => 'VARCHAR(100)',
         'description' => 'VARCHAR(32000)'
     }
-
-    conn.execute <<SQL
-DROP TABLE test_model_sql;
-SQL
-    File.delete('development.db')
   end
 
+  def test_create
+    TestModelSQL.create(submitter: 'Olga', link: 'http://google.com', description: 'Lorem ipsum', posted: 1)
+    TestModelSQL.create(submitter: 'Olga', link: 'http://google.com', description: 'Lorem ipsum', posted: 1)
+
+    assert_equal TestModelSQL.count, 2
+  end
+
+  def test_to_sql_numeric
+    assert_kind_of String, TestModelSQL.to_sql(5)
+  end
+
+  def test_to_sql_string
+    assert_equal TestModelSQL.to_sql('test'), 'test'
+  end
+
+  def test_to_sql_raising_error
+   assert_raise(RuntimeError) { TestModelSQL.to_sql Object.new }
+  end
 
 end
