c8b622a28435e17bd484f86712d5203a5a89a9f6
Olga Grabek <ola@codequest.eu>
2013-12-28 07:56:50 +0100

    add create and count methods

--- lib/shoebill/sql_model.rb
+++ lib/shoebill/sql_model.rb
@@ -11,6 +11,45 @@ module Shoebill
     # Man class for SQLite model.
     class SQLite
 
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+          when Numeric
+            val.to_s
+          when String
+            "#{val}"
+          else
+            raise "Can't change #{val.class} to SQL"
+        end
+      end
+
+      def self.create(values)
+        values.delete 'id'
+        keys = schema.keys - ['id']
+        vals = keys.map do |key|
+          values[key] ? to_sql(values[key]) : 'null'
+        end
+
+        DB.execute <<SQL
+INSERT INTO #{table} (#{keys.join(',')})
+VALUES (#{vals.join(',')});
+SQL
+
+        data = Hash[keys.zip vals]
+        sql = "SELECT last_insert_rowid();"
+        data['id'] = DB.execute(sql)[0][0]
+        self.new data
+      end
+
+      def self.count
+        DB.execute(<<SQL)[0][0]
+SELECT COUNT(*) FROM #{table};
+SQL
+      end
+
       # Returns class underscore name.
       # === Example
       #   class MyModel < SQLite
