2dbac39d1910b7161fdac4eaa23cab95d1c18980
Olga Grabek <ola@codequest.eu>
2014-01-07 06:16:29 +0100

    modify routing

--- lib/shoebill/routing.rb
+++ lib/shoebill/routing.rb
@@ -1,6 +1,32 @@
+class RouteObject
+
+  def initialize
+    @rules = []
+  end
+
+  def match(url, *args)
+
+  end
+
+  def check_url(url)
+
+  end
+
+end
+
 module Shoebill
 
   class Application
+
+    def route(&block)
+      @route_obj ||= RouteObject.new
+      @route_obj.instance_eval &block
+    end
+
+    def get_rack_app(env)
+      raise 'No routes!' unless @route_obj
+      @route_obj.check_url env['PATH_INFO']
+    end
 
     # Takes controller and action name from env variable.
     # Returns array with controller object and action name.
