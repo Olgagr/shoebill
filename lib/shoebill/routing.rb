6d374fa828b3c438f89c8e05af2e4209bd191fd1
Olga Grabek <ola@codequest.eu>
2014-01-10 06:50:02 +0100

    add new routing methods

--- lib/shoebill/routing.rb
+++ lib/shoebill/routing.rb
@@ -13,11 +13,63 @@ class RouteObject
     dest = args.pop if args.size > 0
     raise 'Too many args' if args.size > 0
 
+    parts = url.split('/')
+    parts.select! { |p| !p.empty? }
 
+    vars = []
+    regexp_parts = parts.map do |part|
+      if part[0] == ':'
+        vars << part[1..-1]
+        '([a-zA-Z0-9]+)'
+      elsif part[0] == '*'
+        vars << part[1..-1]
+        '(.*)'
+      else
+        part
+      end
+    end
+
+    regexp = regexp_parts.join('/')
+    @rules.push({
+                    regexp: Regexp.new("^/#{regexp}$"),
+                    vars: vars,
+                    dest: dest,
+                    options: options
+                })
   end
 
   def check_url(url)
+    @rules.each do |r|
+      m = r[:regexp].match(url)
 
+      if m
+        options = r[:options]
+        params = options[:default].dup
+        r[:vars].each_with_index do |v,i|
+          params[v] = m.captures[i]
+        end
+
+        dest = nil
+        if r[:dest]
+          return get_dest(r[:dest], params)
+        else
+          controller = params['controller']
+          action = params['action']
+          return get_dest("#{controller}##{action}", params)
+        end
+      end
+    end
+    nil
+  end
+
+  def get_dest(dest, routing_params = {})
+    return dest if dest.respond_to?(:call)
+    if dest =~ /^([^#]+)#([^#]+)$/
+      name = $1.capitalize
+      controller = Object.const_get("#{name}Controller")
+      return controller.action($2, routing_params)
+    end
+    raise "No destination: #{dest.inspect}"
   end
 
 end
