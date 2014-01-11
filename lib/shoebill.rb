b42c5515d8644720ad879f97a0f4f6057155f2c0
Olga Grabek <ola@codequest.eu>
2014-01-11 17:23:00 +0100

    remove redundant code

--- lib/shoebill.rb
+++ lib/shoebill.rb
@@ -18,28 +18,9 @@ module Shoebill
         return [404, {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, act = get_controller_and_action(env)
-      rack_app = klass.action(act)
+      rack_app = get_rack_app(env)
       rack_app.call(env)
 
-      #if controller.get_response
-      #  st, hd, rs = controller.get_response.to_a
-      #  [st, hd, [rs.body].flatten]
-      #else
-      #  controller.render(action)
-      #end
-
-
-      #klass, action = get_controller_and_action(env)
-      #controller = klass.new(env)
-      #controller.send(action)
-      #
-      #if controller.get_response
-      #  st, hd, rs = controller.get_response.to_a
-      #  [st, hd, [rs.body].flatten]
-      #else
-      #  controller.render(action)
-      #end
     end
 
   end
